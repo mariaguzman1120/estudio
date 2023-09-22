554e343cabef0271dc22e1b3a27e07bf31c66421
jhanva <jarbey.mejia@gmail.com>
2023-09-22 13:43:11 -0500

    refactor: organiza semestre del repo

---
+++ semestre_i/fundamentos_programacion/notebooks/quiz_2/vistas.py
@@ -0,0 +1,31 @@
+# Johan Arbey Mejía Vargas cc: 1152195956
+# María del mar Ipia Guzmán cc: 1214726595
+
+import os
+
+from io import open
+
+
+if __name__ == '__main__':
+    path = os.path.join('..', 'data', 'visitas.txt')
+    fichero = open(path, 'r', encoding='utf-8')
+    texto = fichero.read()
+    fichero.close()
+
+    if not texto:
+        texto = 0
+    else:
+        texto = int(texto)
+
+    argumento = input('ingrese argumento: ')
+
+    if argumento == 'inc':
+        texto += 1
+    elif argumento == 'dec':
+        texto -= 1
+    else:
+        print(texto)
+
+    fichero = open(path, 'w')
+    fichero.write(str(texto))
+    fichero.close()
